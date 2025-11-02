8842ab0800e3b87897e4a2624a26fb544af753e7
Iyan Nazarian <iyan.nazarian@etu.u-paris.fr>
2025-11-02 14:20:55 +0100

    feat(parse): added utils to read from big/little endian to host byte order

--- erraid/include/utils.h
+++ erraid/include/utils.h
@@ -2,7 +2,15 @@
 # define UTILS_H
 
 # include <stdio.h>
+# include <stdbool.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <inttypes.h>
+# include <sys/endian.h>
+
 
 void	print_help(void);
+bool	is_little_endian(void);
 
 #endif
