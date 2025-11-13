e0f9ff5b53f90b18c2fafe287271316dc8e02b51
vincentf <florian.vincent@etu.u-paris.fr>
2025-11-13 19:00:32 +0100

    tests in main should output the right thing

--- erraid/src/main.c
+++ erraid/src/main.c
@@ -5,6 +5,7 @@
 #include "utils/utils.h" // IWYU pragma: keep
 #include "parser/parse_tasks.h"
 #include "structs.h"
+#include "exec/exec.h"
 
 // if passed data is made on little-endian architecture
 // set by parser()
@@ -22,6 +23,14 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 
 	print_cmd_tree(ctx.tasks->cmd);
+
+	printf("\nExecution\n");
+	if (execute_command(ctx.tasks->cmd)) {
+		printf("Command executed successfully\n");
+		printf("Exit code: %d\n", ctx.tasks->cmd->exit_code);
+	} else {
+		printf("Command execution failed\n");
+	}
 
 	free_tasks(ctx.tasks);
 
