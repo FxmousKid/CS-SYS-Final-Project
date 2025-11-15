775f0916993859547ab4447bc08b2397813eecb7
NAZARIAN iyan <iyan.nazarian@etu.u-paris.fr>
2025-11-15 18:46:39 +0100

    Merging multi-task into exec

--- erraid/src/main.c
+++ erraid/src/main.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 
 #include "parser/parse_cli.h"
-#include "commands/parse_cmd_tree.h"
 #include "utils/utils.h" // IWYU pragma: keep
 #include "parser/parse_tasks.h"
 #include "structs.h"
@@ -12,32 +11,66 @@
 // set by parser()
 bool	isdle;
 
-void	exec_test(struct s_task *tasks)
+
+
+// /**
+//  * @brief Builds complete paths to stdout and stderr files of a given task
+//  */
+// static void build_output_paths(const char *task_path, char *stdout_path, char *stderr_path)
+// {
+// 	if (!build_safe_path(stdout_path, PATH_MAX+1, task_path, STDOUT_FILE))
+// 		ERR_MSG("Failed to build stdout path");
+// 	if (!build_safe_path(stderr_path, PATH_MAX+1, task_path, STDERR_FILE))
+// 		ERR_MSG("Failed to build stderr path");
+// }
+
+void	exec_test(struct s_data *ctx)
 {
-	char stdout_path[PATH_MAX + 1];
-	char stderr_path[PATH_MAX + 1];
+	struct s_task *current_task;
+	int task_count;
+	int task_id;
 
-	printf("\nExecution\n");
-	stdout_path[0] = '\0';
-	strncat(stdout_path, tasks->path, PATH_MAX);
-	strncat(stdout_path, STDOUT_FILE, PATH_MAX - strlen(stdout_path));
+	// Debug lines
+	printf("Found %d tasks\n", count_sub_cmds(ctx->run_directory));
+	printf("Execution\n");
+	
+	current_task = ctx->tasks;
+	task_count = 0;
+	
+	while (current_task) {
+		
+		task_id = current_task->task_id;
 
-	stderr_path[0] = '\0';
-	strncat(stderr_path, tasks->path, PATH_MAX);
-	strncat(stderr_path, STDERR_FILE, PATH_MAX - strlen(stderr_path));
+		// Debug lines 
+		printf("\n Executing Task %d\n", task_id);
+		printf("Path: %s\n", current_task->path);
+		print_cmd_tree(current_task->cmd);
+		
+		// build_output_paths(current_task->path, path_to_stdout, path_to_stderr);
+		printf("Output files:\n");
+		printf("  stdout: %s\n", current_task->stdout_path);
+		printf("  stderr: %s\n", current_task->stderr_path);
 
-	if (exec_cmd_with_redir(tasks->cmd, stdout_path, stderr_path)) {
-		printf("Command executed successfully\n");
-		printf("Exit code: %d\n", tasks->cmd->exit_code);
-	} else {
-		printf("Command execution failed\n");
+		if (exec_cmd_with_redir(current_task->cmd, current_task->stdout_path, current_task->stderr_path)) {
+			// Debug lines
+			printf("Task %d executed successfully\n", task_id);
+			printf("Exit code: %d\n", current_task->cmd->exit_code);
+		} else {
+			// Debug lines
+			printf("Task %d execution failed\n", task_id);
+		}
+		
+		current_task = current_task->next;
+		task_count++;
 	}
+	// Debug lines
+	printf("\nTotal: executed %d tasks\n", task_count);
 }
+
 
 int main(int argc, char *argv[])
 {
 	struct s_data	ctx = {0};
-	// struct s_task	*task = NULL;
 
 	parser_cli(&ctx, argc, argv);
 	isdle = ctx.is_data_le;
@@ -45,13 +78,8 @@ int main(int argc, char *argv[])
 	if (!parse_tasks(&ctx))
 		return EXIT_FAILURE;
 
-	print_cmd_tree(ctx.tasks->cmd);
+	exec_test(&ctx);
 
-	/* Exec tests */
-	exec_test(ctx.tasks);
-
-	/* End of exec tests */
 	free_tasks(ctx.tasks);
-
 	return EXIT_SUCCESS;
 }
