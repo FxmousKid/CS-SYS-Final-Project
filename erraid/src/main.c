d433e50cb496c2a8cd23c8c7ef6a3087092c611c
florian <florian@debian>
2025-11-15 12:13:44 +0100

    stderr and stdout files are w orking with redirection

--- erraid/src/main.c
+++ erraid/src/main.c
@@ -7,6 +7,7 @@
 #include "structs.h"
 #include "exec/exec.h"
 
+
 // if passed data is made on little-endian architecture
 // set by parser()
 bool	isdle;
@@ -15,6 +16,8 @@ int main(int argc, char *argv[])
 {
 	struct s_data	ctx = {0};
 	// struct s_task	*task = NULL;
+	char path_to_stdout[PATH_MAX + 1];
+	char path_to_stderr[PATH_MAX + 1];
 
 	parser_cli(&ctx, argc, argv);
 	isdle = ctx.is_data_le;
@@ -23,14 +26,26 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 
 	print_cmd_tree(ctx.tasks->cmd);
+	
 
+	/* Exec tests */
 	printf("\nExecution\n");
-	if (execute_command(ctx.tasks->cmd)) {
+	path_to_stdout[0] = '\0';
+	strncat(path_to_stdout, ctx.tasks->path, PATH_MAX);
+	strncat(path_to_stdout, "stdout", PATH_MAX - strlen(path_to_stdout));
+
+	path_to_stderr[0] = '\0';
+	strncat(path_to_stderr, ctx.tasks->path, PATH_MAX);
+	strncat(path_to_stderr, "stderr", PATH_MAX - strlen(path_to_stderr));
+
+	if (exec_cmd_with_redir(ctx.tasks->cmd, path_to_stdout, path_to_stderr)) {
 		printf("Command executed successfully\n");
 		printf("Exit code: %d\n", ctx.tasks->cmd->exit_code);
 	} else {
 		printf("Command execution failed\n");
 	}
+
+	/* End of exec tests */
 
 	free_tasks(ctx.tasks);
 
