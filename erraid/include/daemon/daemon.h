d059fbcaa6cd6b734226db2636638fdef772cebe
vincentf <florian.vincent@etu.u-paris.fr>
2025-11-16 20:13:00 +0100

    signal handlers

--- erraid/include/daemon/daemon.h
+++ erraid/include/daemon/daemon.h
@@ -7,7 +7,11 @@
 #include <sys/types.h>
 #include <stdbool.h>
 #include <utils/utils.h>
+#include <macros.h>
+#include <signal.h>
 
 bool daemonize();
+bool is_daemon_running();
+void setup_sig_handlers();
 
 #endif
--- erraid/src/daemon/daemon.c
+++ erraid/src/daemon/daemon.c
@@ -1,5 +1,30 @@
 # include "daemon/daemon.h"
 
+static volatile sig_atomic_t running = 1;
+
+static void sig_handler(int sig) 
+{
+        (void)sig; // For warning (sig not used)
+        running = 0;
+}
+
+void setup_sig_handlers()
+{
+        struct sigaction sa;
+
+	sa.sa_handler = sig_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+}
+
+bool is_daemon_running()
+{
+        return running;
+}
 
 /**
  * @brief daemonize the process (see man 7 daemon for every steps)
