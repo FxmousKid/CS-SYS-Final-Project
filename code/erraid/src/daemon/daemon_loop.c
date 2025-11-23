c8c2167130c7051b3f8b8a042e1e7a1e531ca319
ShinraOnFire <shinraonfire@mailfence.com>
2025-11-23 20:25:46 +0100

    hotfix: all tests passed

--- code/erraid/src/daemon/daemon_loop.c
+++ code/erraid/src/daemon/daemon_loop.c
@@ -123,21 +123,24 @@ static void     exec_tasks_loop(struct s_data *ctx)
 
 void    daemon_loop(struct s_data *ctx)
 {
-	time_t	last_execution ;
+	time_t	next_execution ;
 	time_t	current_time;
-
-	last_execution = 0;
+	time_t now;
 
 	setup_sig_handlers();
 
+	now = time(NULL);
+	// Get rid of seconds (to start on 00 seconds)
+	next_execution = ((now / 60) + 1) * 60;
+	
 	while (is_daemon_running()) {
 
 		current_time = time(NULL);
 
 		// Check every minute if a task can be executed
-		if (current_time - last_execution >= 60) {
-			last_execution = current_time;
+		if (current_time >= next_execution) {
 			ctx->debug_mode ? exec_tasks_loop_debug(ctx) : exec_tasks_loop(ctx);
+			next_execution = ((current_time/60) + 1) * 60;
 		}
 		// Wait 1 sec, maybe change it to 30s or 60s.
 		sleep(1);
