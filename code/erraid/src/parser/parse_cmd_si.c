b6a8ae4b44557c0578dc0ecbd4b18815e69be513
vincentf <florian.vincent@etu.u-paris.fr>
2025-12-15 16:24:53 +0100

    convert the cmd path to an absolute path to exec external relative commands (bcs daemonize do chdir(/) )

--- code/erraid/src/parser/parse_cmd_si.c
+++ code/erraid/src/parser/parse_cmd_si.c
@@ -84,6 +84,8 @@ bool	parse_cmd_si(const char path[PATH_MAX + 1], struct s_cmd *cmd)
 {
 	int		fd;
 	char		buf[PATH_MAX + 1] = {0};
+	char		resolved_path[PATH_MAX + 1] = {0};
+
 
 	strcpy(buf, path);
 	if (buf[strlen(buf) - 1] != '/')
@@ -105,8 +107,18 @@ bool	parse_cmd_si(const char path[PATH_MAX + 1], struct s_cmd *cmd)
 		return false;
 	}
 
-	if (find_binary_path(cmd->cmd.cmd_si.command[0], cmd->cmd.cmd_si.cmd_path) == false) {
-		ERR_MSG("binary not found");
+	if (convert_to_absolute_path(cmd->cmd.cmd_si.command[0], resolved_path)) {
+		strlcpy(cmd->cmd.cmd_si.cmd_path, resolved_path, sizeof(cmd->cmd.cmd_si.cmd_path));
+	}
+	else if (cmd->cmd.cmd_si.command[0][0] != '/') {
+		if (!find_binary_path(cmd->cmd.cmd_si.command[0], cmd->cmd.cmd_si.cmd_path)) {
+			ERR_MSG("binary not found in $PATH: %s", cmd->cmd.cmd_si.command[0]);
+			close(fd);
+			return false;
+		}
+	}
+	else {
+		ERR_MSG("Failed to resolve absolute path for command %s", cmd->cmd.cmd_si.command[0]);
 		close(fd);
 		return false;
 	}
