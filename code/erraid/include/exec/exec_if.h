45c20d4570d56d8c46af5b0353adb81d5672dbcf
NAZARIAN iyan <iyan.nazarian@etu.u-paris.fr>
2026-01-02 02:16:09 +0100

    chore: adding execution files for CMD_IF (barebones)

---
+++ code/erraid/include/exec/exec_if.h
@@ -0,0 +1,17 @@
+#ifndef EXEC_IF_H
+# define EXEC_IF_H
+
+# include <stdbool.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+# include "structs.h" // IWYU pragma: keep
+# include "utils/utils_exec.h" // IWYU pragma: keep
+# include "utils/utils.h" // IWYU pragma: keep
+# include "utils/binary_utils.h" // IWYU pragma: keep
+
+
+
+
+#endif
