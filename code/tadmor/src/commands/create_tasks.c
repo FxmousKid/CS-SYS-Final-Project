c5f89727c5768b831444f1dac6f38e8a8c78abf3
VINCENT florian <florian.vincent@etu.u-paris.fr>
2026-01-10 14:08:52 +0100

    forgot a conflict

--- code/tadmor/src/commands/create_tasks.c
+++ code/tadmor/src/commands/create_tasks.c
@@ -7,11 +7,7 @@ static bool	request_create_tasks(struct s_data *ctx)
 
 	buffer_init(&buf, INITIAL_BUF_CAPACITY);
 	buffer_append_uint16(&buf, OPCODE_CR);
-<<<<<<< HEAD
-	// buffer_append_uint* already converts to big-endian
-=======
 	// buffer_append_uint already converts to big-endian
->>>>>>> eb8fe82 (cancelled the combine changes, but fixed the endianness for timing)
 	buffer_append_uint64(&buf, ctx->cmd.timing.minutes);
 	buffer_append_uint32(&buf, ctx->cmd.timing.hours);
 	buffer_append(&buf, &ctx->cmd.timing.days, 1);
